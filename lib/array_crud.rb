63aefc7c7cb6c9a2497bc396e1e3080448a6c087
Lessllie Real <rlessllie@yahoo.com>
2019-08-11 17:29:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
-  
+  my_pets=["dog","cat", "fish", "bird"]
+  "dog"=my_pets.shift
 end
 
 def retrieve_element_from_index(array, index_number)
